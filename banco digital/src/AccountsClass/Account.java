6b0bb95750a217095b00efd6fd96c59fbe13ab39
LuanArves <luanspsoares@outlook.com>
2023-06-19 09:32:48 -0300

    Implementing the account interface class and also implementing the classes Account, Current Account, and Savings Account.

--- banco digital/src/AccountsClass/Account.java
+++ banco digital/src/AccountsClass/Account.java
@@ -1,4 +1,15 @@
 package AccountsClass;
 
 public class Account {
+
+    private String id;
+    private int numero_conta, numero_agencia;
+    private double saldo;
+
+    public Account(String id, int numero_conta, int numero_agencia, double saldo) {
+        this.id = id;
+        this.numero_conta = numero_conta;
+        this.numero_agencia = numero_agencia;
+        this.saldo = saldo;
+    }
 }
